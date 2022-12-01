6a5c1d3c9476942661ee0ef88d74936c318dffb4
thiag0nery <thiagonery456@gmail.com>
2022-12-01 01:34:34 -0300

    A

--- Electus/dados/dados/src/main/resources/static/Electus_Scripts/principalEstudante.js
+++ Electus/dados/dados/src/main/resources/static/Electus_Scripts/principalEstudante.js
@@ -56,3 +56,28 @@ function testeScroll(){
     
     
 }
+let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider div")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
